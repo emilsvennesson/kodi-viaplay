f05f6294fe5153dc30d799a11326bee7d3317223
Mariusz89B <mariusz.brychcy@outlook.com>
2023-01-10 21:21:15 +0100

    v2.3.7

--- login.py
+++ login.py
@@ -1,9 +1,12 @@
 import sys
 from resources.lib.kodihelper import KodiHelper
 
+helper = KodiHelper()
+
 class Login:
     def __init__(self):
-        KodiHelper().authorize(autologin=True)
+        if helper.get_setting('autologin'):
+            helper.authorize(autologin=True)
 
 if __name__ == '__main__':
     r = Login()
